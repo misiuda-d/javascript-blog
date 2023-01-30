cd32450146d83f5459ee872b5ccec1e1057982e1
Dominika Misiuda <misiuda.d@gmail.com>
2023-01-30 21:42:03 +0100

    build html code in js

--- js/script.js
+++ js/script.js
@@ -1,50 +1,102 @@
 'use strict';
 
-function titleClickHandler(event){
+function titleClickHandler(event) {
     event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
-    
 
-  /* [DONE] remove class 'active' from all article links  */
 
-  const activeLinks = document.querySelectorAll('.titles a.active');
+    /* [DONE] remove class 'active' from all article links  */
 
-for(let activeLink of activeLinks){
-  activeLink.classList.remove('active');
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
+    }
+
+    /* [DONE] add class 'active' to the clicked link */
+
+    console.log('clickedElement:', clickedElement);
+    clickedElement.classList.add('active');
+
+    /* [DONE] remove class 'active' from all articles */
+
+    const activeArticles = document.querySelectorAll('.posts .active');
+
+    for (let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
+    }
+
+    /* [DONE] get 'href' attribute from the clicked link */
+
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log('articleSelector: ', articleSelector);
+
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
+
+    /* [DONE] add class 'active' to the correct article */
+
+    targetArticle.classList.add('active');
+
 }
 
-  /* [DONE] add class 'active' to the clicked link */
 
-  console.log('clickedElement:', clickedElement);
-  clickedElement.classList.add('active');
+const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
 
-  /* [DONE] remove class 'active' from all articles */
+function generateTitleLinks() {
 
-  const activeArticles = document.querySelectorAll('.posts .active');
+    /* [DONE] remove contents of titleList */
 
-  for(let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
-  }
+    const titleList = document.querySelector(optTitleListSelector);
+    console.log('Deleted list: ', titleList);
+    titleList.innerHTML = '';
 
-  /* [DONE] get 'href' attribute from the clicked link */
+    /* for each article */
 
-  const articleSelector = clickedElement.getAttribute('href');
-  console.log('articleSelector: ', articleSelector); 
+    const articles = document.querySelectorAll(optArticleSelector);
+    let html = '';
 
-  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+    for (let article of articles) {
 
-  const targetArticle = document.querySelector(articleSelector);
-  console.log(targetArticle);
+        /* get the article id */
 
-  /* [DONE] add class 'active' to the correct article */
+        const articleId = article.getAttribute('id');
+        console.log('articleId: ', articleId);
+    }
 
-  targetArticle.classList.add('active');
+    /* find the title element */
 
+    const articleTitleElement = article.querySelector(optTitleSelector);
+
+
+    /* get the title from the title element */
+
+    const articleTitle = articleTitleElement.innerHTML;
+
+    /* create HTML of the link */
+
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log('linkHTML: ', linkHTML);
+
+    /* insert link into titleList */
+
+    insertAdjacentHTML('afterbegin', linkHTML);
+    html = html + linkHTML;
+    console.log('html: ', html);
 }
+
+
+titleList.innerHTML = html;
+
+generateTitleLinks();
 
 const links = document.querySelectorAll('.titles a');
-
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
-}
+  console.log('links: ', links);
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
