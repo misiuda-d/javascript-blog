4d1cac5722a480d7bdff7ecc5daad22fb68d205a
Dominika Misiuda <misiuda.d@gmail.com>
2023-02-03 21:23:40 +0100

    generate taglinks

--- js/script.js
+++ js/script.js
@@ -46,7 +46,8 @@
 }
     const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
-    optTitleListSelector = '.titles';
+    optTitleListSelector = '.titles',
+    optArticleTagsSelector = '.post-tags .list';
 
     function generateTitleLinks() {
 
@@ -97,9 +98,45 @@
 
     generateTitleLinks();
 
+    function generateTags(){
 
+    /* find all articles */
 
+    const articles = document.querySelectorAll('.post');
+    console.log(articles);
 
+    /* START LOOP: for every article: */
 
+    for (let article of articles) {
 
+      /* find tags wrapper */
 
+    const tagsWrapper = article.querySelector(optArticleTagsSelector);
+
+      /* make html variable with empty string */
+
+    let html = '';
+
+      /* get tags from data-tags attribute */
+
+    const articleTags = article.getAttribute('.post-tags .list');
+    console.log(articleTags);
+
+      /* split tags into array */
+
+    const articleTagsArray = articleTags.split(' ');
+
+        /* START LOOP: for each tag */
+    for(let tag of articleTagsArray) {
+
+          /* generate HTML of the link */
+
+          /* add generated code to html variable */
+    }
+        /* END LOOP: for each tag */
+
+        /* insert HTML of all the links into the tags wrapper */
+
+      /* END LOOP: for every article: */
+    }
+    generateTags();
